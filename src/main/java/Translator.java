3a1fa09d0b6cab83e85fbe8a0a876065b9cd8a9d
simonqv <simonqv@kth.se>
2021-05-06 12:44:22 +0200

    Testar sofias git

--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -3,4 +3,16 @@
  * Pig Latin
  */
 public class Translator {
+
+    /**
+     * Method to translate into rövarspråket.
+     * @param s the tweet to translate
+     * @return translated string builder
+     */
+    public StringBuilder rovarspraket(String s) {
+        // translate here
+        return null;
+
+    }
+
 }
