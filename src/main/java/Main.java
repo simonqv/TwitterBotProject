597b3ef51b280619f0b659b610d8be0d8f0f1717
simonqv <simonqv@kth.se>
2021-05-06 13:26:17 +0200

    Implemeted translator for Rövarsråket

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,11 +15,11 @@ public class Main {
     public static void main(String[] args) throws TwitterException {
         Tweeter t = new Tweeter();
         //System.out.println(t.postTweet("totesostot"));
-        // Har lite problem med git...
         TweetReader tr = new TweetReader();
         List<Status> l = tr.getTimeLine();
-        for (var s : l) {
-            System.out.println(s.getUser().getName() + " " + s.getText());
-        }
+        //for (var s : l) {
+        //    System.out.println(s.getUser().getName() + " " + s.getText());
+        //}
+        var s = new Translator().rovarspraket("Hej jag är hungrig!");
     }
 }
--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Rövarspråket
  * Pig Latin
@@ -6,13 +10,42 @@ public class Translator {
 
     /**
      * Method to translate into rövarspråket.
-     * @param s the tweet to translate
+     * @param tweet the tweet to translate
      * @return translated string builder
      */
-    public StringBuilder rovarspraket(String s) {
-        // translate here
-        return null;
+    public String rovarspraket(String tweet) {
 
+        List<String> list = Arrays.asList(tweet.split("(?!^)"));
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).matches("^[qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM]$")) {
+                list.set(i, list.get(i) + "o" + list.get(i).toLowerCase());
+            }
+        }
+
+        System.out.println(list);
+        StringBuilder sb = new StringBuilder();
+        for (String string : list) {
+            sb.append(string);
+        }
+
+/*
+        //var newString = tweet.replaceAll("^[eyuioåaöäEYUIOÅÄÖA]$");
+
+        System.out.println(list);
+
+
+        List<String> rovar = list.stream().map(s -> {
+            if (!s.matches("^[eyuioåaöäEYUIOÅÄÖA]$") ) {
+                s.replace(s, s + "o" + s);
+                System.out.println(s);
+            }
+            return s;
+        }
+        ).collect(Collectors.toList());
+////   System.out.println(list);
+*/
+        return sb.toString();
     }
 
 }
