cbce752bad7bdac7adf4cee268142129356cc112
Sofia Eriksson <sofierik@kth.se>
2021-05-07 11:54:45 +0200

    clean up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,17 +13,16 @@ import java.util.List;
 
 public class Main {
 
-    //test
-    // test 2
-
     public static void main(String[] args) throws TwitterException {
         Tweeter t = new Tweeter();
         //System.out.println(t.postTweet("totesostot"));
         TweetReader tr = new TweetReader();
         List<Status> l = tr.getTimeLine();
-        //for (var s : l) {
-        //    System.out.println(s.getUser().getName() + " " + s.getText());
-        //}
+
+        for (var s : l) {
+            System.out.println(s.getLang() + ": " + s.getText());
+            System.out.println(s.getUser().getName() + " " + s.getText());
+        }
         //var s = new Translator().toRovarspraket("Hej jag är hungrig!");
         //var s = new Translator().toPiglatin("Please, shut your mouth and hide your teeth!");
         //System.out.println(s);
