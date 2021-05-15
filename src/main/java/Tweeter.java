dd88fdd8a23540e08804098bcef10a9d11de0d69
Sofia Eriksson <sofierik@kth.se>
2021-05-15 14:45:35 +0200

    Finish postTweet

--- src/main/java/Tweeter.java
+++ src/main/java/Tweeter.java
@@ -1,8 +1,6 @@
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,28 +16,28 @@ public class Tweeter {
         this.t = t;
     }
 
-    public String postTweet(List<TranslatedStatus> ts) throws TwitterException {
+    public void postTweet(List<TranslatedStatus> ts) throws TwitterException {
         Twitter twitter = new TwitterFactory().getInstance();
+        StringBuilder sb = parser(ts);
 
-        // Vad vill vi göra?
-        // steg 1: Vi vill ta in tweetet
-        // vad behöver vi?
-        // * själva tweeten. user. (eventually vill vi typ ha positiv eller negativ?? eller bild)
+        if (!ts.get(0).getStatus().getUser().toString().equals("bandole19183971")) {
+            if (sb.length() > 280) {
+                twitter.updateStatus(sb.toString());
+            } else {
+                List<String> subTweetList = tweetSplitter(sb);
+                long inReplyToStatusId = -1;
+                int counter = 0;
 
+                while (!subTweetList.isEmpty()) {
+                    StatusUpdate statusUpdate = new StatusUpdate(Integer.toString(counter));
+                    statusUpdate.setInReplyToStatusId(inReplyToStatusId);
 
-
-        // steg 2: vi vill kolla hur långt det är
-
-        // steg 2.5: dela upp om det är för lpngt
-
-        // steg 3: tweeta
-
-
-        // var len = statusList.get(1);
-        //if ()
-        //Status status = twitter.updateStatus(tweet);
-        //return status.getText();
-        return null;
+                    Status updatedStatus = twitter.updateStatus(statusUpdate);
+                    inReplyToStatusId = updatedStatus.getId();
+                    counter++;
+                }
+            }
+        }
     }
 
     // Status first, Translated second
@@ -53,7 +51,26 @@ public class Tweeter {
         } else {
             sb.append(" totwoweetotadode: ");
         }
-
         return sb.append(ts.get(1).getTranslatedTweet());
+    }
+
+    private List<String> tweetSplitter(StringBuilder sb) {
+        int min = 0;
+        int max = 280;
+        List<String> subTweetList = new ArrayList<>();
+        while (max < sb.length()) {
+            if (sb.length() - min - 1 < 280) {
+                subTweetList.add(sb.substring(min, sb.length() - 1));
+            } else {
+                if (String.valueOf(sb.charAt(max)).equals(" ")) {
+                    max--;
+                } else {
+                    subTweetList.add(sb.substring(min, max));
+                    min = max;
+                    max += 280;
+                }
+            }
+        }
+        return subTweetList;
     }
 }
