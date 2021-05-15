8cdf0c841f0761bd26cd4b6eb887928e03740eb2
Sofia Eriksson <sofierik@kth.se>
2021-05-15 13:46:05 +0200

    Tweeter, problem igen

--- src/main/java/Tweeter.java
+++ src/main/java/Tweeter.java
@@ -5,6 +5,9 @@ import twitter4j.TwitterFactory;
 
 import java.util.List;
 
+/**
+ * This class is responsible of posting tweets on the twitter feed, using the translators translated tweets.
+ */
 public class Tweeter {
 
     // private final List<Status> statusList;
@@ -19,16 +22,38 @@ public class Tweeter {
         Twitter twitter = new TwitterFactory().getInstance();
 
         // Vad vill vi göra?
-        // Vi vill ta in tweetet
-        // vi vill kolla hur långt det är
-        // dela upp om det är för lpngt
-        // tweeta
+        // steg 1: Vi vill ta in tweetet
+        // vad behöver vi?
+        // * själva tweeten. user. (eventually vill vi typ ha positiv eller negativ?? eller bild)
+
+
+
+        // steg 2: vi vill kolla hur långt det är
+
+        // steg 2.5: dela upp om det är för lpngt
+
+        // steg 3: tweeta
 
 
         // var len = statusList.get(1);
         //if ()
         //Status status = twitter.updateStatus(tweet);
         //return status.getText();
-            return null;
+        return null;
+    }
+
+    // Status first, Translated second
+    private StringBuilder parser(List<TranslatedStatus> ts) {
+        Status tweetInfo = ts.get(0).getStatus();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(tweetInfo.getUser().toString());
+        if (tweetInfo.getLang().equals("en")) {
+            sb.append(" Eetedtway: ");
+        } else {
+            sb.append(" totwoweetotadode: ");
+        }
+
+        return sb.append(ts.get(1).getTranslatedTweet());
     }
 }
